0242f12f6592632b979ec7daabff8efb633f3a75
UmarAslamNawab <umar_uni321@outlook.com>
2020-04-08 18:09:23 +0500

    new modifications

---
+++ index(7-4-20).js
@@ -0,0 +1,79 @@
+// var name = prompt("what is your name?")
+
+// if(name === "Maryam"){
+//     alert("Welcome to the class")
+// }
+// else{
+//     alert("Sorry you enter into the wrong class")
+// }
+
+// var sub1= +prompt("Enter your first subject marks?")
+// var sub2= +prompt("Enter your second subject marks?")
+// var sub3= +prompt("Enter your third subject marks?")
+
+// var totalMarks=300
+// var marksObt= sub1 + sub2 + sub3
+// var percentage = marksObt * 100 / totalMarks
+
+
+// if(percentage>= 80){           
+//     alert("Your grade is A")
+// }
+
+// if(percentage >=70){           
+//     alert("Your grade is B")
+// }
+
+// if(percentage >= 60){           
+//     alert("Your grade is C")
+// }
+
+
+//2
+
+// if(percentage >= 80){
+//     alert("Your Grade is A")
+// }
+// else{
+//  alert("your grade is not A")   
+// }
+
+// if(percentage >=70){
+//     alert("your grade is B")
+// }
+// else{
+//     alert("your grade is not B")
+// }
+
+// if(percentage >= 60){
+//     alert("Your grade is C")
+// }
+// else{
+//     alert("Sorry you're fail")
+// }
+
+
+// //3
+var sub1= +prompt("Enter your first subject marks?")
+var sub2= +prompt("Enter your second subject marks?")
+var sub3= +prompt("Enter your third subject marks?")
+
+var totalMarks=300
+var marksObt= sub1 + sub2 + sub3
+var percentage = marksObt * 100 / totalMarks
+
+
+if(percentage >= 80){
+    alert("Your Grade is A")
+}
+
+else if(percentage >=70){
+    alert("your grade is B")
+}
+else if(percentage >= 60){
+    alert("Your grade is C")
+}
+else{
+    alert("Sorry you're fail")
+}
+
---
+++ index(8-4-20).js
@@ -0,0 +1,71 @@
+// var weight = +prompt("enter weight?")
+// var time = +prompt("enter time?")
+// var age = +prompt("enter age?")
+// var gender = prompt("enter your gender?")
+
+
+
+// if (weight > 300 && time < 6 && age > 17 && gender === "male") {
+// alert("Come to our tryout!");
+// }
+// else {
+// alert("Come to our cookout!");
+// }
+
+
+
+// var age = 12
+// var res= "US"
+
+// if ( (age > 65 || age < 21) && res === "US") {
+// alert("CONGO")
+// }
+// else{
+//     alert("Sorry")
+// }
+
+
+
+/** Logical operators **/
+
+        // AND - &&
+        // OR - ||
+        // NOT - !
+
+        //        true && true   = true
+        //        false && true  = false
+        //        false && false = false
+        //        true && false  = false
+
+        //true || true   = true
+        //false || true  = true
+        //false || false = false
+        //true || false  = true
+
+
+/** Ternary Operator **/
+
+
+        // var isConfirmed = true;
+        // var status;
+        // if (isConfirmed == true) {
+        //     status = "Confirmed";
+        // }
+        // else {
+        //     status = "Pending";
+        // }
+        // alert(status)
+
+
+        // var isConfirmed = false;
+        // var status;
+        // status = isConfirmed != true ? "Confirmed" : "Pending";
+        // alert(status)        
+
+
+
+var country = prompt("What is your country?")
+var city= prompt("What is your city?")
+var district= prompt("what is your district?")    
+
+
