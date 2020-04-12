f7ce88c4539dfa7a6a9d02d0ad5f56a0d8429d4d
UmarAslamNawab <umar_uni321@outlook.com>
2020-04-12 17:05:10 +0500

    new changes

---
+++ index(12-4-20).js
@@ -0,0 +1,95 @@
+
+// var country = prompt("What is your country?")
+// var province= prompt("what is your Province?")    
+// var city= prompt("What is your city?")
+
+//Pakistan SA America Turkey India
+
+// if(country === "Pak"){
+//     if(province === "Sindh"){
+//         if(city === "Karachi"){
+//             document.write("You can contact your administrator for the allocation of funds")
+//         }
+//         else if(city === "Larkana"){
+//             document.write("You can contact your administrator for the allocation of funds")
+
+//         }
+//         else if(city === "NosheroFerouz"){
+//             document.write("You can contact your administrator for the allocation of funds")
+
+//         }
+
+//         else if(city === "Thatta"){
+//             document.write("You can contact your administrator for the allocation of funds")
+
+//         }
+//         else{
+//             document.write("Sorry you enter the wrong city of Sindh")
+//         }
+
+
+//     }
+//     else if(province === "KPK"){
+
+//     }
+//     else if(province === "Balochistan"){
+
+//     }
+//     else if(province === "Punjab"){
+
+//     }
+//     else{
+//         document.write("May you have enter the wrong  province of PAK")
+//     }
+
+// }
+// else if(country === "Turkey"){
+
+// }
+// else if(country === "America"){
+
+// }
+// else if(country === "SA"){
+
+// }
+// else if(country === "India"){
+
+// }
+// else{
+//     document.write("Sorry You will be treated in the second stage")
+// }
+
+
+
+
+// if(country === "Pak" && (province === "Sindh" || province === "Punjab" || province === "KPK" || province === "Balochistan") && (city === "Karachi" || city === "Larkana"|| city === "Thatta" ||city === "NF" )){
+//     document.write("Contact your administrator")
+// }
+// else{
+//     document.write("Sorry you are not a part of this")
+// }
+
+
+// if(position === "developer" && skills === "JS" && (age > 17 || age <21)){
+//     document.write("Come for the interview session next Monday")
+// }
+// else{
+//     document.write("Sorry we not have the correct position for you")
+// }
+
+
+//Arrays
+//length=1 index=0
+
+
+// var cities = ["Atlanta", "Baltimore", "Chicago", "Denver", "Los Angeles", "Seattle"];
+// alert("Welcome to " + cities[5])
+
+// var name = "Umar"
+// var rollNo = 445
+// var num = "+92 333 3660070"
+// var trvFromVan = true
+
+var stdData = ["Umar"  , "+92 333 3660070" , 445 , true]
+
+alert(stdData[3])
