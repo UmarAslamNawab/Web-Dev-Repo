6db6eaf8c2b590b3b83e99416a6b3f46d49ca1cb
UmarAslamNawab <umar_uni321@outlook.com>
2020-04-25 02:41:13 +0500

    practice code

---
+++ index(25-4-20).js
@@ -0,0 +1,104 @@
+// var num = prompt
+// var length = prompt
+
+// for(var i=1 ; i<=length ; i++){
+//     document.write(num + "x " + i + "=" + num*i)      5 x 1 = 5
+// }
+
+
+
+// var input = prompt
+// var Array= []
+// for(i ; i<Array.length ; i++){
+//     if(Array[i] === input){
+//         document("" + i + input)
+//         break
+//     }
+//     else{
+//         document("sorry")
+//     }
+    
+// }
+
+
+// var arrayNum = [3 , 6, 2, 56, 32, 5, 89, 32];
+// var largNum= 0;
+
+// for (i=0; i<=6;i++){
+//     if (56>largNum) {
+//          largNum=array[i];
+//     }
+// }
+
+
+
+
+// console.log(largNum);
+
+
+
+var city = "KARACHI"
+var user = "karAchI"
+
+//convert user input into small case //karachi
+
+if(user === "karachi")
+
+
+
+
+
+
+
+// var num = prompt
+// var length = prompt
+
+// for(var i=1 ; i<=length ; i++){
+//     document.write(num + "x " + i + "=" + num*i)      5 x 1 = 5
+// }
+
+
+
+// var input = prompt
+// var Array= []
+// for(i ; i<Array.length ; i++){
+//     if(Array[i] === input){
+//         document("" + i + input)
+//         break
+//     }
+//     else{
+//         document("sorry")
+//     }
+    
+// }
+
+
+// var arrayNum = [3 , 6, 2, 56, 32, 5, 89, 32];
+// var largNum= 0;
+
+// for (i=0; i<=6;i++){
+//     if (56>largNum) {
+//          largNum=array[i];
+//     }
+// }
+
+
+
+
+// console.log(largNum);
+
+
+
+var city = "KARACHI"
+var user = "karAchI"
+
+//convert user input into small case //karachi
+
+if(user === "karachi")
+
+
+
+
+
+
+
