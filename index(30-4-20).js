b95cda0abd6f7663798f276538464ba8ddcb34cd
UmarAslamNawab <umar_uni321@outlook.com>
2020-04-30 16:44:23 +0500

    number methods

---
+++ index(30-4-20).js
@@ -0,0 +1,32 @@
+var num = 26.5
+document.write(num + "<br>")
+
+var newNum = Math.round(76.5)
+
+document.write(newNum + "<br>")
+
+// var scoreAvg = Math.round(.0678437);
+// console.log(scoreAvg)
+
+// var scoreAvg = Math.ceil(-1.000001);
+// console.log(scoreAvg)
+
+// -3    -2      -1      0   1   2    3
+
+// var scoreAvg = Math.floor(.299999);
+// console.log(scoreAvg)
+
+var randomNumber = Math.random();
+document.write("<h1>"+ randomNumber +"</h1>")
+
+// var newRand = randomNumber * 10000000000000000
+// document.write("<h1>"+ newRand +"</h1>")
+
+var newRand =(randomNumber * 6) + 1
+document.write("<h1>"+ newRand +"</h1>")
+
+var newRound =Math.floor(newRand)
+document.write("<h1>"+ newRound +"</h1>")
+
+
+5.9999999999999994 + 1 // 6.99..... ===> 7
