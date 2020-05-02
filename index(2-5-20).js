9b99be3bb076e206b4ad55cb1ef5b7b38c84c73c
UmarAslamNawab <umar_uni321@outlook.com>
2020-05-03 02:47:13 +0500

    number methods

---
+++ index(2-5-20).js
@@ -0,0 +1,31 @@
+// var profit = "200" - "duck";
+// console.log(profit)
+
+// var result = "200" - 150;
+// console.log(result)
+
+// var currentAge = +prompt("Enter your age.");
+// var currentAge = parseInt(prompt("Enter your age."));
+// var currentAge = parseFloat(prompt("Enter your age."));
+// var qualifyingAge = currentAge + 1;
+// console.log(qualifyingAge)  
+
+// var myInteger = parseInt("1.9999");
+// console.log(myInteger)
+
+// var myFractional = parseFloat("1.9999");
+// console.log(myFractional)
+
+// var num = "12345.897"
+// var newNum= Number(num)
+// console.log(newNum)
+
+
+// var num = 12345.897
+// var newNum= num.toString()
+// console.log(newNum)
+
+var num = +prompt("Enter any number")
+var newNum= num.toFixed()
+console.log(newNum)
+// console.log(num)
