4020ce17b68edebdb6f04c67ec519690fe80ca1a
UmarAslamNawab <umar_uni321@outlook.com>
2020-05-31 17:05:58 +0500

    DOM code

---
+++ 31-5-20/index.js
@@ -0,0 +1,17 @@
+// var d = document.getElementById("div3");
+// var p = d.getElementsByTagName("p");
+// var contents = p[2].innerHTML;
+// console.log(contents)
+
+// var p = document.getElementsByTagName("p"); //5
+// var contents = p[4].innerHTML;
+// console.log(contents)
+
+// var d= document.getElementById("ny") //div
+// var p = d.getElementsByTagName("p"); //2
+// var contents = p[1].innerHTML;
+// console.log(contents)
+
+var p = document.childNodes[0]
+// var contents = p.innerHTML;
+console.log(p)
