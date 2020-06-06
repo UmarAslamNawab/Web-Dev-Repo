a9e090744adcef284311b603ac4c6d93f2855806
UmarAslamNawab <umar_uni321@outlook.com>
2020-06-06 16:24:47 +0500

    DOM code

---
+++ 6-6-20/index(6-6-20).js
@@ -0,0 +1,47 @@
+// var p = document.childNodes[0].childNodes[2].childNodes[3].childNodes[3].childNodes[0]
+// // var contents = p.innerHTML;
+// console.log(p)
+
+
+// var parentNode= document.getElementById("ny")
+// var target=parentNode.lastChild
+// console.log(target)
+
+// var targert2 = document.getElementById("ny")
+// var result=targert2.parentNode
+// console.log(result)
+
+// var targert3 = document.getElementById("cal")
+// var result=targert3.nextSibling
+// console.log(result)
+
+// var targert4 = document.getElementById("ny")
+// var result=targert4.previousSibling
+// console.log(result)
+
+// var targert5 = document.getElementById("cal")
+// var result=targert5.nodeName
+// console.log(result)
+
+// var targert6 = document.getElementById("cal")
+// var result=targert6.nodeType
+// console.log(result)
+
+
+
+
+// var targert7 = document.getElementById("fp").firstChild
+// var result=targert7.nodeValue
+// console.log(targert7)
+
+
+
+// var targert8 = document.getElementsByTagName("p")
+// var result=targert8.length
+// console.log(targert8)
+
+
+
+var targert9 = document.getElementById("cal")
+var result=targert9.childNodes
+console.log(result)
